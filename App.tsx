5c6cdcd1184d87bc768d8406ed7dcbcd17f74bb6
Gaspard Chevassus <chevassusgaspard@gmail.com>
2021-04-01 12:53:42 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ const App = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Read the docs to discover what to do next
           </Section>
           <LearnMoreLinks />
         </View>
